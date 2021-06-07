0fae6ca214f9ba6045c2b1de8cf4da6e65ff5727
Carlos Dorneles <carlos.dorneles@luizalabs.com>
2021-06-07 00:02:25 -0300

    Fix database router problem

--- src/project/core/databases.py
+++ src/project/core/databases.py
@@ -1,3 +1,6 @@
+import os
+
+
 class DatabaseRouter:
     """
     A router to control all database operations on models in the
@@ -5,14 +8,19 @@ class DatabaseRouter:
     https://docs.djangoproject.com/en/3.2/topics/db/multi-db/
     """
 
-    @staticmethod
-    def db_for_read(model, **hints):
+    def __init__(self):
+        self.settings = (
+            os.getenv('SIMPLE_SETTINGS') or
+            os.getenv('DJANGO_SETTINGS_MODULE')
+        )
+
+    def db_for_read(self, model, **hints):
+        if self.settings == 'project.core.settings.test':
+            return 'default'
         return 'default_read'
 
-    @staticmethod
-    def db_for_write(model, **hints):
+    def db_for_write(self, model, **hints):
         return 'default'
 
-    @staticmethod
-    def allow_relation(obj1, obj2, **hints):
+    def allow_relation(self, obj1, obj2, **hints):
         return True
